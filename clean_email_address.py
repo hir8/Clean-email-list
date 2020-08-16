f0b84f9a9bf8ce268e71b426b034003fc4dd6ff3
Clips <hiroito@Clipss-MBP.lan>
2020-08-16 01:51:55 -0300

    added coments

--- clean_email_address.py
+++ clean_email_address.py
@@ -7,6 +7,7 @@ data.drop(columns=['Team','Organization','Phone','Location'], inplace=True)
 # Drop empty email addresses
 data.dropna(subset=['Email'], inplace=True)
 # Search for addresses gmail or hotmail, .com or .ca
+# Change it to match your desired email address
 print(data[data['Email'].str.contains(r'^[A-Za-z0-9._%+-]+@(gmail|hotmail).(com|ca)$', regex=True)])
 
 
